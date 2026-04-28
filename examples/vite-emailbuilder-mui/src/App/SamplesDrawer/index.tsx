55febd0eef35eaae5570ebb7ebbb95ccd5ca7dea
usaofnj <info@usaofnj.org>
2026-04-27 23:17:18 -0400

    Update index.tsx

--- examples/vite-emailbuilder-mui/src/App/SamplesDrawer/index.tsx
+++ examples/vite-emailbuilder-mui/src/App/SamplesDrawer/index.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
-
-import { Box, Button, Divider, Drawer, Link, Stack, Typography } from '@mui/material';
-
+import { Drawer, Stack, Typography } from '@mui/material';
 import { useSamplesDrawerOpen } from '../../documents/editor/EditorContext';
-
 import SidebarButton from './SidebarButton';
-import logo from './waypoint.svg';
 
 export const SAMPLES_DRAWER_WIDTH = 240;
 
 export default function SamplesDrawer() {
   const samplesDrawerOpen = useSamplesDrawerOpen();
-
   return (
     <Drawer
       variant="persistent"
@@ -21,12 +16,11 @@ export default function SamplesDrawer() {
         width: samplesDrawerOpen ? SAMPLES_DRAWER_WIDTH : 0,
       }}
     >
-      <Stack spacing={3} py={1} px={2} width={SAMPLES_DRAWER_WIDTH} justifyContent="space-between" height="100%">
+      <Stack spacing={3} py={1} px={2} width={SAMPLES_DRAWER_WIDTH} height="100%">
         <Stack spacing={2} sx={{ '& .MuiButtonBase-root': { width: '100%', justifyContent: 'flex-start' } }}>
           <Typography variant="h6" component="h1" sx={{ p: 0.75 }}>
-            EmailBuilder.js
+            USA Email Builder
           </Typography>
-
           <Stack alignItems="flex-start">
             <SidebarButton href="#">Empty</SidebarButton>
             <SidebarButton href="#sample/welcome">Welcome email</SidebarButton>
@@ -38,40 +32,6 @@ export default function SamplesDrawer() {
             <SidebarButton href="#sample/post-metrics-report">Post metrics</SidebarButton>
             <SidebarButton href="#sample/respond-to-message">Respond to inquiry</SidebarButton>
           </Stack>
-
-          <Divider />
-
-          <Stack>
-            <Button size="small" href="https://www.usewaypoint.com/open-source/emailbuilderjs" target="_blank">
-              Learn more
-            </Button>
-            <Button size="small" href="https://github.com/usewaypoint/email-builder-js" target="_blank">
-              View on GitHub
-            </Button>
-          </Stack>
-        </Stack>
-        <Stack spacing={2} px={0.75} py={3}>
-          <Link href="https://usewaypoint.com?utm_source=emailbuilderjs" target="_blank" sx={{ lineHeight: 1 }}>
-            <Box component="img" src={logo} width={32} />
-          </Link>
-          <Box>
-            <Typography variant="overline" gutterBottom>
-              Looking to send emails?
-            </Typography>
-            <Typography variant="body2" color="text.secondary" paragraph>
-              Waypoint is an end-to-end email API with a &apos;pro&apos; version of this template builder with dynamic
-              variables, loops, conditionals, drag and drop, layouts, and more.
-            </Typography>
-          </Box>
-          <Button
-            variant="contained"
-            color="primary"
-            sx={{ justifyContent: 'center' }}
-            href="https://usewaypoint.com?utm_source=emailbuilderjs"
-            target="_blank"
-          >
-            Learn more
-          </Button>
         </Stack>
       </Stack>
     </Drawer>
