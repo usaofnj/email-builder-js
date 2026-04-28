7218ea75d9f3c1d2322812536ceafbb9a6c3c939
usaofnj <info@usaofnj.org>
2026-04-27 23:57:43 -0400

    Update index.tsx

--- examples/vite-emailbuilder-mui/src/App/SamplesDrawer/index.tsx
+++ examples/vite-emailbuilder-mui/src/App/SamplesDrawer/index.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { Drawer, Stack, Typography } from '@mui/material';
 import { useSamplesDrawerOpen } from '../../documents/editor/EditorContext';
 import SidebarButton from './SidebarButton';
-
 export const SAMPLES_DRAWER_WIDTH = 240;
-
 export default function SamplesDrawer() {
   const samplesDrawerOpen = useSamplesDrawerOpen();
   return (
@@ -23,14 +21,7 @@ export default function SamplesDrawer() {
           </Typography>
           <Stack alignItems="flex-start">
             <SidebarButton href="#">Empty</SidebarButton>
-            <SidebarButton href="#sample/welcome">Welcome email</SidebarButton>
-            <SidebarButton href="#sample/one-time-password">One-time passcode (OTP)</SidebarButton>
-            <SidebarButton href="#sample/reset-password">Reset password</SidebarButton>
-            <SidebarButton href="#sample/order-ecomerce">E-commerce receipt</SidebarButton>
-            <SidebarButton href="#sample/subscription-receipt">Subscription receipt</SidebarButton>
-            <SidebarButton href="#sample/reservation-reminder">Reservation reminder</SidebarButton>
-            <SidebarButton href="#sample/post-metrics-report">Post metrics</SidebarButton>
-            <SidebarButton href="#sample/respond-to-message">Respond to inquiry</SidebarButton>
+            <SidebarButton href="#sample/welcome">USA Newsletter</SidebarButton>
           </Stack>
         </Stack>
       </Stack>
